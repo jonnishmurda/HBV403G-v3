5a0069484e2d09551abe4150bcadb88aa541276d
jonnishmurda <jjo1@hi.is>
2025-03-20 19:35:18 +0000

    whaa

--- src/index.ts
+++ src/index.ts
@@ -197,7 +197,7 @@ app.post('/questions/:id/answer', async (c) => {
 
 serve({
   fetch: app.fetch,
-  port: 5000
+  port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 });
